2e847525820465d6468143a555f0e5dbd5806e7a
Thiên Toán <toanalien@gmail.com>
2014-12-21 08:17:56 +0700

    app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+
+var http = require('http');
+
+var options = {
+  host: 'abc.com',
+  port: 80,
+  path: '/',
+  method: 'GET'
+};
+
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+req.write('data\n');
+req.write('data\n');
+req.end();
+
+
+var myServer = http.createServer(function(req,res){
+	res.writeHead(200);
+	res.end();
+}).listen(3000);
